10b5edadfbc7c564cbb31e2a70647277aaba48c2
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-07-20 23:54:06 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+var request = require("request");
+var fs = require('fs');
+var writedir = "../angular-app/src/assets/json/";
+
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
+    next();
+});
+
+router.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
+});
+function update(sheet) {
+    var writeto = writedir + sheet + ".json";
+    request({
+        uri: "https://spreadsheets.google.com/feeds/list/1mK2zvp6ouN5r7kh0StqkULr32l9MyO0suBpMJUL4QeM/" + sheet + "/public/values?alt=json",
+        method: "GET",
+        timeout: 20000,
+        followRedirect: true,
+        maxRedirects: 10
+    }, function (error, response, body) {
+        if(!body){console.log(error)}
+            fs.writeFile(writeto, body.feed.entry);
+    });
+}
+
+router.post("/:sheet", function (req, res) {
+    update(req.params.sheet);
+    res.send('200');
+});
+app.use("/", router);
+
+app.listen(3000, function () {
+    console.log("Live at Port 3000");
+});
