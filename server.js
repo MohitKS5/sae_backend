1c5febea52fc551d21a033f4257b3d6f89220f47
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-09-24 09:55:02 +0530

    resolve port error

--- server.js
+++ server.js
@@ -34,6 +34,12 @@ router.post("/:sheet", function (req, res) {
 });
 app.use("/", router);
 
-app.listen(3000, function () {
-    console.log("Live at Port 3000");
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
